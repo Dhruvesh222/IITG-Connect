a76353dacdc1058decc4996d49f8c7fb378676d7
Dhruvesh Bhure <84266782+Dhruvesh222@users.noreply.github.com>
2022-01-06 14:30:18 +0530

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -25,19 +25,6 @@ module.exports = function(passport){
     }))
 
 
-
-    passport.use(new GoogleStrategy({
-        clientID: '329819064124-ilethg06apk1se237pg48jhspuluqdfu.apps.googleusercontent.com',
-        clientSecret: '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',
-        callbackURL: "http://localhost:5000/google/callback",
-        passReqToCallback: true
-      },
-      function(accessToken, refreshToken, profile, done) {
-        console.log(profile);
-        return done(null,profile);
-      }
-    ));
-
     passport.serializeUser((user, done) => {
         done(null, user.id);
       });
